3bcba642fdbbc523c2ae963deaf716a6589a7946
AndruLyashenko <laavllag85@gmail.com>
2017-02-05 00:21:58 +0400

    'javasckri'

--- js/main.js
+++ js/main.js
@@ -23,24 +23,26 @@ map();
 for (var moved_4 = 0; moved_4 < 13; moved_4++) {
     if (isFree('south') == true) {
         south();
-    } else {
+    }
+    else {
         east();
     }
     map();
 }
 
 // 5 level
-function level_5() {
+function moved_5() {
     var step = '';
     while (step != 'next') {
         if ((isFree('east') == true) && (isFree('north') == false) && (isFree('south') == false)) {
             step = east();
             map();
-        } else {
+        }
+        else {
             step = south();
             map();
         }
-        if ((isFree('east')!=true) && (isFree('south')!=true)) {
+        if ((isFree('east') != true) && (isFree('south') != true)) {
             break;
         }
     }
@@ -57,7 +59,7 @@ function level_5() {
             step = west();
             map();
         }
-        if ((isFree('west')!=true) && (isFree('south')!=true)) {
+        if ((isFree('west') != true) && (isFree('south') != true)) {
             break;
         }
     }
@@ -70,7 +72,7 @@ function level_5() {
             step = east();
             map();
         }
-        if ((isFree('north')!=true) && (isFree('east')!=true)) {
+        if ((isFree('north') != true) && (isFree('east') != true)) {
             break;
         }
     }
@@ -84,7 +86,7 @@ function level_5() {
             step = south();
             map();
         }
-        if ((isFree('west')!=true) && (isFree('south')!=true)) {
+        if ((isFree('west') != true) && (isFree('south') != true)) {
             break;
         }
     }
@@ -104,155 +106,10 @@ function level_5() {
     }
 }
 
-level_5();
+moved_5();
 
 
 
 
 
 
-
-
-//first-step//
-function first_level() {
-    var step = '';
-    while (step != 'next') {
-        step = north();
-        console.log(step);
-    }
-
-}
-
-first_level()
-
-
-//second-step//
-function second_level() {
-    var step = '';
-    while (step != 'next') {
-        step = east();
-        console.log(step);
-    }
-}
-second_level()
-
-//third(fourth)-step//
-
-function third_level() {
-    var step = '';
-    while (step != 'next') {
-        if (isFree('south')) {
-            step = south();
-        }
-        else {
-            step = east();
-            console.log(step);
-
-        }
-        if (!step) {
-
-            break;
-        }
-
-    }
-
-}
-
-third_level()
-
-//4-step//
-
-function third_level() {
-    var step = '';
-    while (step != 'next') {
-        if (isFree('south')) {
-            step = south();
-        }
-        else {
-            step = east();
-            console.log(step);
-
-        }
-        if (!step) {
-            break;
-        }
-
-    }
-
-}
-
-third_level()
-
-//fifth-step//
-function fifth_level() {
-    for (var step = ''; step !='next'; ) {
-        if (isFree('east') && isFree('north') == false && isFree('south') == false) {
-            step=east();
-        }
-        else {
-            step=south();
-        }
-        if (!isFree('east') && !isFree('south')) {
-            break;
-        }
-
-    }
-
-    for (; step !='next'; ) {
-        if (isFree('west')) {
-            step=west();
-        }
-        else if (isFree('west') == false) {
-            step=south();
-        }
-        else  {
-            step=west();
-        }
-        if (!isFree('west') && !isFree('south')) {
-            break;
-        }
-
-    }
-
-    for (; step !='next'; ) {
-        if (isFree('north')) {
-            step=north();
-        }
-        else  {
-            step=east();
-        }
-        if (!isFree('north') && !isFree('east')) {
-            break;
-        }
-    }
-
-    for (; step !='next'; ){
-        if (isFree('west')){
-            step=west();
-        }
-        else{
-            step=south();
-        }
-        if (!isFree('west') && isFree('south')) {
-            break;
-        }
-    }
-
-    for (; step!='next'; ) {
-
-        if (isFree('south')) {
-            step=south();
-        }
-        else if (isFree('east')) {
-            step= east();
-            console.log(step);
-        }
-        if (!step) {
-
-            break;
-        }
-    }
-
-}
-
-fifth_level()
